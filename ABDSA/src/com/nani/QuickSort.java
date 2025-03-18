0f3d96306b1b35ff172f9091b8c9c999f3783dbc
muralinaveen <muralinaveen906@gmail.com>
2025-03-18 17:48:57 +0530

    quciksort

--- ABDSA/src/com/nani/QuickSort.java
+++ ABDSA/src/com/nani/QuickSort.java
@@ -11,10 +11,47 @@ public class QuickSort {
 		   arr[i]=scan.nextInt();	
 				   
 	   }
-	   quickSort(arr,0,arr.length-1);
-	   System.out.println(Arrays.toString(arr));
+	  // quickSort(arr,0,arr.length-1);
+	   quickmawa(arr,0,arr.length);
+	  System.out.println(Arrays.toString(arr));
+	  
 	   
 }
+ 
+ 
+ public static void quickmawa(int []arr,int low ,int high) {
+	
+	 if(low<high) {
+		 int indexmawa=partionIndexmawa(arr,low,high);
+	 quickmawa(arr,low,indexmawa-1);
+	 quickmawa(arr,indexmawa+1,high);
+	 }
+ }
+ public static int partionIndexmawa(int []arr,int low ,int high) {
+	 
+	 int i=low;
+	 int j=high;
+	 int pivot=arr[low];
+	 while(i<j) {
+		 while(i<=high-1 && arr[i]<=pivot) {
+			 i++;
+		 }
+		 while(j>=low+1 && arr[j]>pivot) {
+			 j--;
+		 }
+		 if(i<j) {
+			 int temp=arr[i];
+			 arr[i]=arr[j];
+			 arr[j]=temp;
+		 }
+	 }
+	 
+	int temp=arr[low];
+	 arr[low]=arr[j];
+	 arr[j]=arr[low];
+	 return j;
+	 
+ }
  public static void quickSort(int []arr,int low ,int high) {
 	 
 	 if(low>=high) {
@@ -35,7 +72,7 @@ public class QuickSort {
 		 }
 		 if(s<=e) {
 			 int temp=arr[s];
-			 arr[s]=arr[e];
+			 arr[s]=arr[e]; 
 			 arr[e]=temp;
 			 s++;
 			 e--;
